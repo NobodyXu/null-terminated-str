5caa0e1af48ea7259f535b49f8cf125e78323e0a
Jiahao XU <Jiahao_XU@outlook.com>
2022-09-05 17:41:21 +1000

    Refactor: Extract new mod `borowed`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ src/borrowed.rs
@@ -0,0 +1,75 @@
+use std::{
+    ffi::CStr,
+    ops::Deref,
+    str::{from_utf8, from_utf8_unchecked, Utf8Error},
+};
+
+const fn report_err() -> u8 {
+    const EMPTY_ARR: [u8; 0] = [];
+    #[allow(unconditional_panic)]
+    EMPTY_ARR[0]
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+pub struct NullTerminatedStr(CStr);
+
+impl NullTerminatedStr {
+    pub fn as_c_str(&self) -> &CStr {
+        &self.0
+    }
+
+    /// # Safety
+    ///
+    /// The `CStr` (excluding the null byte)
+    /// must be valid utf-8 str.
+    pub const unsafe fn from_cstr_unchecked(cstr: &CStr) -> &Self {
+        // Safety: NullTerminatedStr is transparent
+        // newtype of CStr
+        &*(cstr as *const CStr as *const Self)
+    }
+
+    pub fn from_cstr(cstr: &CStr) -> Result<&Self, Utf8Error> {
+        from_utf8(cstr.to_bytes())?;
+        Ok(unsafe { Self::from_cstr_unchecked(cstr) })
+    }
+
+    /// This function creates a `NullTerminatedStr`
+    /// from `s` which must have only one null byte
+    /// at the end of the string.
+    ///
+    /// If not, then this function would panic.
+    pub const fn from_const_str(s: &str) -> &Self {
+        let bytes = s.as_bytes();
+
+        if bytes.is_empty() {
+            report_err();
+        }
+
+        let mut i = 0;
+        let n = bytes.len() - 1;
+
+        // Check last byte is null byte
+        if bytes[n] != b'\0' {
+            report_err();
+        }
+
+        // Ensure there is no internal null byte.
+        while i < n {
+            if bytes[i] == b'\0' {
+                report_err();
+            }
+            i += 1;
+        }
+
+        unsafe { Self::from_cstr_unchecked(CStr::from_bytes_with_nul_unchecked(bytes)) }
+    }
+}
+
+impl Deref for NullTerminatedStr {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { from_utf8_unchecked(self.0.to_bytes()) }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,7 @@
-use std::{
-    ffi::{CStr, CString},
-    ops::Deref,
-    str::{from_utf8, from_utf8_unchecked, Utf8Error},
-};
+use std::{ffi::CString, ops::Deref};
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(transparent)]
-pub struct NullTerminatedStr(CStr);
-
-impl NullTerminatedStr {
-    pub fn as_c_str(&self) -> &CStr {
-        &self.0
-    }
-
-    /// # Safety
-    ///
-    /// The `CStr` (excluding the null byte)
-    /// must be valid utf-8 str.
-    pub const unsafe fn from_cstr_unchecked(cstr: &CStr) -> &Self {
-        // Safety: NullTerminatedStr is transparent
-        // newtype of CStr
-        &*(cstr as *const CStr as *const Self)
-    }
-
-    pub fn from_cstr(cstr: &CStr) -> Result<&Self, Utf8Error> {
-        from_utf8(cstr.to_bytes())?;
-        Ok(unsafe { Self::from_cstr_unchecked(cstr) })
-    }
-}
-
-const fn report_err() -> u8 {
-    const EMPTY_ARR: [u8; 0] = [];
-    #[allow(unconditional_panic)]
-    EMPTY_ARR[0]
-}
-
-impl NullTerminatedStr {
-    /// This function creates a `NullTerminatedStr`
-    /// from `s` which must have only one null byte
-    /// at the end of the string.
-    ///
-    /// If not, then this function would panic.
-    pub const fn from_const_str(s: &str) -> &Self {
-        let bytes = s.as_bytes();
-
-        if bytes.is_empty() {
-            report_err();
-        }
-
-        let mut i = 0;
-        let n = bytes.len() - 1;
-
-        // Check last byte is null byte
-        if bytes[n] != b'\0' {
-            report_err();
-        }
-
-        // Ensure there is no internal null byte.
-        while i < n {
-            if bytes[i] == b'\0' {
-                report_err();
-            }
-            i += 1;
-        }
-
-        unsafe { Self::from_cstr_unchecked(CStr::from_bytes_with_nul_unchecked(bytes)) }
-    }
-}
+mod borrowed;
+pub use borrowed::NullTerminatedStr;
 
 /// Create a null-terminated utf-8 str as an rvalue.
 /// Appends a NUL byte to the passed string.
@@ -100,14 +35,6 @@ macro_rules! const_null_terminated_str {
     ($strval:expr) => {
         $crate::NullTerminatedStr::from_const_str(concat!($strval, "\0"))
     };
-}
-
-impl Deref for NullTerminatedStr {
-    type Target = str;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { from_utf8_unchecked(self.0.to_bytes()) }
-    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
